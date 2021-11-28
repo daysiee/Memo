27dd204e975f7fe0324d5241e67719e2a2195d6a
전다희 <daysiee@naver.com>
2021-11-29 00:09:09 +0900

    테이블 셀 구성

--- Memo/Model/Memo.m
+++ Memo/Model/Memo.m
@@ -21,7 +21,7 @@
     Memo *memo1 = [[Memo alloc] initWithContent:@"내일 뭐먹지"];
     Memo *memo2 = [[Memo alloc] initWithContent:@"6시에 일어나자"];
     
-    return [NSArray arryWithObjects:memo1, memo2, nil];
+    return [NSArray arrayWithObjects:memo1, memo2, nil];
 } // 팩토리 메소드
 
 @end
