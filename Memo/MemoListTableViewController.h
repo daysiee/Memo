72d97af70133c640ccf01822dced9ff10d1a1e6a
전다희 <daysiee@naver.com>
2021-11-29 00:18:46 +0900

    Table VC(뷰 컨트롤러) 생성: VC 클래스 생성 및 씬과 연결

---
+++ Memo/MemoListTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  MemoListTableViewController.h
+//  Memo
+//
+//  Created by 전다희 on 2021/11/29.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MemoListTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
